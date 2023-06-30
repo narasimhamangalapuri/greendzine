036142320c30e2cc3327fcc0d782c90f07b3ebc9
narasimhamangalapuri <129071546+narasimhamangalapuri@users.noreply.github.com>
2023-06-30 15:02:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1');
+      setUsers(response.data.data);
+    } catch (error) {
+      console.log('Error fetching users:', error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search by first name"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div>
+        {filteredUsers.map((user) => (
+          <div key={user.id}>
+            <img src={user.avatar} alt={user.first_name} />
+            <p>{user.first_name}</p>
+            <p>{user.email}</p>
+            <p>{user.last_name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
